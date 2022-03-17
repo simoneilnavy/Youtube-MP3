5ca562f1d9dadcd98b9d7fc1a97055ad63bed887
simoneilnavy <91974671+simoneilnavy@users.noreply.github.com>
2022-03-17 09:42:22 +0100

    Inizialize project

---
+++ Youtube-MP3/Youtube-MP3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+
+namespace Youtube_MP3
+{
+    class Downloader
+    {
+        private string link;
+        
+        public Downloader(string link)
+        {
+            this.link = link;
+        }
+        private bool validatore() 
+        {
+            var youtube= new YoutubeClient();
+
+            if (true)
+            {
+                
+            }           
+            return true;
+        }
+    }
+}
