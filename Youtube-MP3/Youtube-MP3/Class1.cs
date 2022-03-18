1aae8e0e8b73e88cd2f2e11222d30ad061059875
Il_navysimone <simonedestro23@gmail.com>
2022-03-18 13:52:08 +0100

    nuove modifiche

--- Youtube-MP3/Youtube-MP3/Class1.cs
+++ Youtube-MP3/Youtube-MP3/Class1.cs
@@ -7,23 +7,51 @@ using YoutubeExplode;
 
 namespace Youtube_MP3
 {
-    class Downloader
-    {
-        private string link;
-        
-        public Downloader(string link)
-        {
-            this.link = link;
-        }
-        private bool validatore() 
-        {
-            var youtube= new YoutubeClient();
+	class Downloader
+	{
+		private List<YoutubeExplode.Videos.Video> VideoList = new List<YoutubeExplode.Videos.Video>();
+		private YoutubeClient youtube = new YoutubeClient();
 
-            if (true)
-            {
-                
-            }           
-            return true;
-        }
-    }
+		public Downloader()
+		{
+			
+		}
+		public void AddList(string link) 
+		{
+			YoutubeExplode.Videos.Video video;
+			try
+			{
+				video = youtube.Videos.GetAsync(link);
+				
+			}
+			catch
+			{
+				throw new Exception("Il video non è raggiungibile");
+				return;
+			}
+
+			VideoList.Add(video);
+			return;
+		}
+		public void Remover(int index)
+		{
+			try
+			{
+				VideoList.RemoveAt(index);
+			}
+			catch
+			{
+				throw new Exception();
+			}
+		}
+		public void DownloadAll() 
+		{
+			string Manifest;
+			foreach(var i in VideoList) 
+			{
+				var streamManifest = youtube.Videos.Streams.GetManifestAsync(Manifest=i.));
+				var stream = youtube.Videos.Streams.DownloadAsync(streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate(), $"video.{streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate().Container}");
+			}
+		}
+	}
 }
--- Youtube-MP3/Youtube-MP3/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Youtube-MP3/Youtube-MP3/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace Youtube_MP3 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Youtube-MP3;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Youtube-MP3;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
